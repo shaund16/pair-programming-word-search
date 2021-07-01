e1030d0f814bd53e9693fcbf147201486afc3d2c
Shaun <s-purslow@hotmail.com>
2021-07-01 00:56:14 +0000

    fix bugs and make base cases and test to confirm it works with mocha

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+////PAIR PROGRAMMING THIS PROJECT WITH MATTHEW MCLEOD/////////
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const verticalJoin = letters.map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+const transpose = function(matrix) {
+  let returnMatrix = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    returnMatrix.push([]);
+  }
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      returnMatrix[col][row] = matrix[row][col];
+    }
+  }
+  return returnMatrix;
+};
+  
+module.exports = wordSearch;
